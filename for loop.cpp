c95c1134dc9454c811842b228b48afbc5d15dc6a
Shirisha <84560175+Siri9014@users.noreply.github.com>
2021-05-24 12:11:58 +0530

    done

---
+++ for loop.cpp
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<ctype.h>
+#include<math.h>
+
+int main ( )
+{
+  int i=0;
+  int j=0;
+  
+  for(i=1;i<=7;i++)
+  {
+  for(j=1;j<i;j++)
+  {
+  printf("01",j);
+}
+printf("\n");
+  }
+  }
+
